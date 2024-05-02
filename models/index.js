dee250b276dfc2f970db7f2d91f82651bf880a0a
Thai Tin Khang <kinyiasgit@gmail.com>
2024-05-02 10:11:42 +0700

    pg

--- models/index.js
+++ models/index.js
@@ -8,16 +8,15 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+const pg = require('pg');
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-    sequelize = new Sequelize(
-      config.database,
-      config.username,
-      config.password,
-      config
-    );
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config,
+    dialectModule: pg,
+  });
 }
 
 fs.readdirSync(__dirname)
